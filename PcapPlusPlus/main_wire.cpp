e5405b5ca1300c1ec3b16366187e2677ea7542af
Amir Hossein Ghadiri <amirpax32@gmail.com>
2023-09-10 16:49:39 +0330

    fix: fxied main_wire.cpp.

--- PcapPlusPlus/main_wire.cpp
+++ PcapPlusPlus/main_wire.cpp
@@ -8,8 +8,28 @@ public:
   Machine() {left = NULL; right = NULL; }
   void set_left(Machine* left) { this->left = left; }
   void set_right(Machine* right) { this->right = right; }
-  void set_packet(const u_char* packet) { this->packet = packet; }
   const u_char* get_packet() { return packet; }
+
+  void send_to_right() {
+    if (right != NULL) {
+      right->recieve_from_left(packet);
+    }
+  }
+  void recieve_from_left(const u_char* packet) { 
+    this->packet = packet;
+    send_to_right();
+  }
+
+  void send_to_left()  {
+    if (left != NULL) {
+      left->recieve_from_right(packet);
+    }
+  }
+  void recieve_from_right(const u_char* packet) { 
+    this->packet = packet;
+    send_to_left();
+  }
+
 protected:
   Machine* left;
   Machine* right;
@@ -19,28 +39,7 @@ protected:
 class Wire : public Machine {
 public:
   Wire() : Machine::Machine() {}
-  void capture_right() {
-    if (right != NULL) {
-      packet = right->get_packet();
-    }
-  }
-  void capture_left()  {
-    if (left != NULL) {
-      packet = left->get_packet();
-    }
-  }
 };
-
-void evoke_wires_to_right(std::vector<Wire>& wires) {
-  for (int i = 1; i < wires.size(); i++) {
-    wires[i].capture_left();
-  }
-}
-void evoke_wires_to_left(std::vector<Wire>& wires) {
-  for (int i = wires.size() - 2; i >= 0; i--) {
-    wires[i].capture_right();
-  }
-}
 
 int main(int argc, char* argv[]) {
 
@@ -53,7 +52,7 @@ int main(int argc, char* argv[]) {
   );
 
   int length = 10;
-  std::vector<Wire> wires(length);
+  std::vector<Machine> wires(length, Wire());
 
   for (int i = 1; i < length; i++) {
     wires[i].set_left(&wires[i - 1]);
@@ -68,16 +67,14 @@ int main(int argc, char* argv[]) {
     struct pcap_pkthdr* packet_header;
 
     if (pcap_next_ex(left, &packet_header, &packet) == 1) {
-      wires[0].set_packet(packet);
-      evoke_wires_to_right(wires);
+      wires[0].recieve_from_left(packet);
       if (pcap_sendpacket(right, wires[length - 1].get_packet(), 100) != 0) {
         std::cout << pcap_geterr(right) << std::endl;
       }
     }
 
     if (pcap_next_ex(right, &packet_header, &packet) == 1) {
-      wires[length - 1].set_packet(packet);
-      evoke_wires_to_left(wires);
+      wires[length - 1].recieve_from_right(packet);
       if (pcap_sendpacket(left, wires[0].get_packet(), 100) != 0) {
         std::cout << pcap_geterr(left) << std::endl;
       }
